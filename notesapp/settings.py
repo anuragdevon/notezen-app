b8f99400ab2f6704f570dd9ab2df8b038eff2bca
anuragdevon <msm19b021@iiitdm.ac.in>
2022-08-12 17:04:56 +0530

    checkpoint: reset_pc

--- notesapp/settings.py
+++ notesapp/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'api.apps.ApiConfig',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +77,8 @@ WSGI_APPLICATION = 'notesapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': os.getenv('DB_NAME'),
     }
 }
 
